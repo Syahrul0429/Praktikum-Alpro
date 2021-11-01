8e7c3e51cec156316a296f64981ba59b527203bc
Syahrul0429 <syahrulromadhani803gmail.com>
2021-11-01 23:16:11 +0800

    Praktikum Alpro

---
+++ MembuatProgramFlowchart.java
@@ -0,0 +1,16 @@
+package flowchart;
+
+public class MembuatProgramFlowchart {
+	public static void main(String args[]) {
+		int n ;
+		n = 0;
+		n = n+1;
+		while(n <= 20) {
+			
+			if (n % 2 == 1) {
+				System.out.println(n + " merupakan bilangan ganjil" );
+			}
+			n++;
+		}
+	}
+}
