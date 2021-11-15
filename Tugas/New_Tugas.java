68ab0cfffefdf588a4b6a2281ef0144d59347eb5
Syahrul0429 <93253954+Syahrul0429@users.noreply.github.com>
2021-11-15 10:51:44 +0800

    Update New_Tugas.java

--- Tugas/New_Tugas.java
+++ Tugas/New_Tugas.java
@@ -11,13 +11,16 @@ public class New_Tugas {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		
-		int n, m;
-		System.out.print("Masukan banyaknya bilangan yang di inginkan : ");
+		int n, m, r;
+		System.out.print("Masukan banyaknya bilangan  : ");
 		n = scan.nextInt();
+		System.out.print("Masukan Bilangan Angka Kelipatan  : ");
+		m = scan.nextInt();
+		
 		
 		for (int a = 1; a<=n;a++){
-			 	m = a*2;
-				System.out.println(" Bilangan Genap Ke-" + a + " Adalah " + m );
+			 	r = a * m;
+				System.out.println(" Bilangan Ke-" + a +" Dengan Kelipatan " + m + " Adalah " + r );
 					
 		}	
 	}
